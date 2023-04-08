cfe847476f0fbd3d782c55ecd920f093573a43b5
Alone <102355876+Himanshu-2728@users.noreply.github.com>
2023-04-08 16:50:38 +0530

    nothing

--- function.py
+++ function.py
@@ -2,7 +2,7 @@ import os
 
 def handleRecClient(conn , addr , path):
     print('Connected')
-    if os.path.exists(path):
+    if (os.path.exists(path)):
         fileName = path.split('/')[-1]
         print(fileName)
         conn.send(str.encode(fileName)) # Sending the name of the file so that a file with the same name can be created in the directory of the user
@@ -16,6 +16,8 @@ def handleRecClient(conn , addr , path):
             conn.send(file.read()) # Sending the binaries of the file
             print('File sent')
 
+    elif(not os.path.exists(path)):
+        print('The path provided is not valid please recheck the path')
 
         
         
--- server.py
+++ server.py
@@ -3,41 +3,46 @@ import os
 from function import handleRecClient
 
 
-
+# Welcome
 print('Hello Welcome to FileShare \nDo you want to share(0) or recieve(1) file')
 method = int(input('Enter 0 or 1: '))
 
 if method == 0:
+        # Creating a server
+
         port = 8080
         host = socket.gethostbyname(socket.gethostname())
         server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-        server.bind(('localhost', port ))
+        server.bind((host, port ))
         server.listen(1)
 
-
+        # Asking for file path 
         print('Please enter the path of the file you want to share')
         path = input("=> ")
         print(path)
 
+        # Waiting until the client connects to the server
         print(f'Waiting for a client to connect\nPlease enter {host} and {port} into the recivers pc')
         conn , addr = server.accept()
         handleRecClient(conn , addr , path)
 
-elif method == 1:
+elif method == 1: # users wants to receive a file
     host = input('Please enter the host name: ')
     port = int(input('Please enter the port: '))
     client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
     client.connect((host, port))
     print('Connected to host')
+
+    # receiving some data about the file
     fileName = client.recv(1024).decode()
     fileSize = int(client.recv(1024).decode())
     fileData = client.recv(fileSize)
-    with open(fileName , 'wb') as file:
-        file.write(fileData)
+    with open(fileName , 'wb') as file: # creating the file if not exist
+        file.write(fileData) # Writing the bytes
 
 
 elif method not in [0 , 1]:
-    print('Fuck you')
+    print('Please enter a number either 1 or 0')
 
 
 
